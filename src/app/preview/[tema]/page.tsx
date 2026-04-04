db4296ac27ac914d34508699fae0957a2d614cde
samani21 <eksloba21@gmail.com>
2026-04-04 22:41:27 +0800

    Update kembali

--- src/app/preview/[tema]/page.tsx
+++ src/app/preview/[tema]/page.tsx
@@ -166,7 +166,7 @@ function PreviewPage() {
                                 {/* Bagian Aksi/Tombol */}
                                 <div className="flex items-center gap-2 w-full md:w-auto">
                                     <Link
-                                        href={'/theme'}
+                                        href={'/'}
                                         className="flex-1 md:flex-none inline-flex items-center justify-center gap-2 px-5 py-2.5 bg-gray-900 hover:bg-gray-800 text-white text-sm font-medium rounded-xl transition-all active:scale-95 shadow-md"
                                     >
                                         <ArrowLeft size={18} />
